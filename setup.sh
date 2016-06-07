038d90771d468e9104da24df7488d430d877a6f4
Mike Chen <yuechuan20@gmail.com>
2016-06-07 16:02:35 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,21 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
 
-#install node and others 
+#add 3rd party repositories  
 sudo add-apt-repository ppa://chris-lea/nbode.js
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-add-repository -y ppa:cassou/emacs
+#chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#anaconda for python 3 
+#forget it, they do not provide a secure apt method 
 
 sudo apt-get update
 
 sudo apt-get install -y make g++ python python-software-properties 
 sudo apt-get install -y nodejs 
+sudo apt-get install -y google-chrome-stable
 
 #install jdk
 sudo apt-get install -y oracle-java8-installer
