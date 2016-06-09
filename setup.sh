790a8ca2c50422cb65b9cfb3bdc74c8cdfc9263b
yuechuan <yuechuan20@gmail.com>
2016-06-09 16:08:10 -0300

    added a tmux plugin

--- setup.sh
+++ setup.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
-# for headless setup. 
 
 #add 3rd party repositories  
 sudo add-apt-repository ppa://chris-lea/nbode.js
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-add-repository -y ppa:cassou/emacs
+
 #chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
 #anaconda for python 3 
 #forget it, they do not provide a secure apt method 
 
 sudo apt-get update
 
-sudo apt-get install -y make g++ python python-software-properties 
+sudo apt-get install -y make g++ python python-software-properties cmake
 sudo apt-get install -y nodejs 
 sudo apt-get install -y google-chrome-stable
+sudo apt-get install -y tmux
+sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
 #install jdk
 sudo apt-get install -y oracle-java8-installer
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 
 #install redshift 
-sudo apt-get install -y redshift redshift-gtk
+#sudo apt-get install -y redshift redshift-gtk
 
 #-version manager
 # https://github.com/creationix/nvm
@@ -49,10 +51,6 @@ git config --global credential.helper 'cache --timeout=3600'
 # See: http://nodejs.org/api/repl.html#repl_repl
 #sudo apt-get install -y rlwrap
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-
-sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
 # git pull and install dotfiles as well
 cd $HOME
@@ -72,4 +70,17 @@ ln -sf dotfiles/.emacs.d .
 #install heroku toolbelt
 #wget -qO- https://toolbelt.heroku.com/install-buntu.sh | sh
 
-#heroku login
+
+
+#set up tmux
+
+cd ~/dotfiles/additional/tmux-mem-cpu/
+cmake .
+make
+sudo make install
+cd 
+touch .tmux.conf
+echo 'set -g status-interval 2' >> .tmux.conf
+echo "set -g status-left \"#S #(tmux-mem-cpu 2 'fg=blue,bg=black,bright')#[default]\"" >> .tmux.conf
+
+
