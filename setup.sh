281fd52d23b1c119de1cb1155b54973d52122e35
Mike Chen <yuechuan20@gmail.com>
2016-08-12 18:04:10 +0000

    merged setup into one file

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y git-core
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 sudo apt-get install -y make g++ cmake
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs npm 
 sudo apt-get install -y sshpass 
 sudo apt-get install -y tmux
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg git-el #for git integration 
@@ -38,10 +38,6 @@ git config --global user.email "yuechuan20@gmail.com"
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
 
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v6.3.1
-nvm use v6.3.1
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -54,6 +50,23 @@ nvm use v6.3.1
 
 #install heroku toolbelt
 #wget -qO- https://toolbelt.heroku.com/install-buntu.sh | sh
+
+# install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/cyc115/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+
 
 
 #set up tmux
